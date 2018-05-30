cbcd48908e03a624ca5b445dd82b691e338f4151
KhanhNguyen <khanh.nguyen.set15@eiu.edu.vn>
2018-05-30 08:49:40 +0700

    Add RecyclerView with sample sites

---
+++ app/src/main/java/com/example/nguyen/tourist_app/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.nguyen.tourist_app;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private PlacesViewModel placesViewModel;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerAdapter;
+    private List<String> touristSites = Arrays.asList("Statue","Scenic Viewpoint","ArtMuseum","City Hall");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerAdapter= new TouristRecyclerAdapter(touristSites);
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        placesViewModel = ViewModelProviders.of(this).get(PlacesViewModel.class);
+//        placesViewModel.getPlacesList().observe(this, new Observer<List<String>>() {
+//            @Override
+//            public void onChanged(@Nullable List<String> places) {
+//                recyclerAdapter = new TouristRecyclerAdapter(places);
+//                recyclerView.setAdapter(recyclerAdapter);
+//            }
+//        });
+
+
+    }
+
+}
+//public class PlacesViewModel extends ViewModel {
+//
+//    private static final List<String> placesList = Arrays.asList("Statue", "Scenic Overlook", "Art Museum",
+//            "Market", "Museum of Natural History", "Temple", "Amusement Park", "City Hall", "Big Bridge");
+//    private LiveData<List<String>> placesListData;
+//
+//    public PlacesViewModel() {
+//        final MutableLiveData<List<String>> data = new MutableLiveData<>();
+//        data.setValue(placesList);
+//        placesListData = data;
+//    }
+//
+//    public LiveData<List<String>> getPlacesList() {
+//        return placesListData;
+//    }
+//}
